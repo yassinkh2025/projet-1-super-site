f991355083adccf4ae0d69b1452bfd309f69726d
yassinkh2025 <yassinkhaldi385@gmail.com>
2025-06-29 17:04:28 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        🚀 Tailwind fonctionne !
+      </h1>
+    </div>
+  );
+}
+
+export default App;
